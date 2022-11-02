06353577bfe2ebc37c47be7a886917714bb90eb6
Sergey <sergsmile29@gmail.com>
2022-11-02 17:09:23 +0200

    wrong data in img source

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import JobSingleItem from './components/JobSingleItem'
 
-function App() {
+export interface SingleItemI {
+	id: string
+	name: string
+	email: string
+	phone: string
+	title: string
+	salary: string
+	address: string
+	description: string
+	createdAt: string
+	pictures: string
+}
+
+const App: React.FC = () => {
+	const [jobList, setJobList] = useState<SingleItemI[]>([])
+	useEffect(() => {
+		fetch(
+			'https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu'
+		)
+			.then(response => response.json())
+			.then(data => setJobList(data))
+	}, [])
 	return (
-		<div className='flex  bg-[#E6E9F2] justify-center'>
+		<div className='flex p-2 bg-[#E6E9F2] justify-center'>
 			<div id='wrapper' className='container py-5 gap-2 flex flex-col'>
-				<JobSingleItem />
-				<JobSingleItem />
-				<JobSingleItem />
-				<JobSingleItem />
-				<JobSingleItem />
-				<JobSingleItem />
-				<JobSingleItem />
+				{jobList.map(item => (
+					<JobSingleItem
+						name={item.name}
+						id={item.id}
+						email={item.email}
+						phone={item.phone}
+						title={item.title}
+						salary={item.salary}
+						address={item.address}
+						description={item.description}
+						createdAt={item.createdAt}
+						pictures={`https://picsum.photos/id/${Math.floor(
+							Math.random() * 100
+						)}/200/300`}
+					/>
+				))}
 			</div>
 		</div>
 	)
--- src/components/JobSingleItem.tsx
+++ src/components/JobSingleItem.tsx
@@ -1,37 +1,45 @@
 import React from 'react'
-import logo from '../assets/img.png'
 import location from '../assets/Location.png'
 import stars from '../assets/stars.png'
 import bookmark from '../assets/Bookmark.png'
-type Props = {}
+import { SingleItemI } from '../App'
 
-const JobSingleItem = (props: Props) => {
+const JobSingleItem = ({
+	email,
+	name,
+	phone,
+	title,
+	salary,
+	address,
+	description,
+	pictures,
+	createdAt,
+}: SingleItemI) => {
 	return (
-		<section className='shadow-md bg-white rounded-md px-3 py-4 flex'>
-			<div className=' min-w-[85px] h-[85px] mr-4'>
-				<img src={logo} alt='jobImg' className='rounded-full' />
+		<section className='shadow-md relative bg-white rounded-md px-3 py-4 flex'>
+			<div className=' max-w-[85px] max-h-[85px] mr-4 overflow-hidden rounded-full'>
+				<img src={pictures} alt='jobImg' className='' />
 			</div>
+
 			<div className='w-[65%]'>
-				<div className='text-[#3A4562] bold text-xl mb-2 '>
-					Arbeitsmediziner/-in / Betriebsmediziner/-in (m/w/d) oder einen
-					Arzt/eine Ärztin (m/w/d) für die Weiterbildung zum Facharzt/ zur
-					Fachärztin für Arbeitsmedizin (m/w/d)
+				<div className='text-[#3A4562] bold text-xl mb-2 hover:cursor-pointer'>
+					{title}
 				</div>
 				<div className='text-[#878D9D] text-md mb-4'>
-					Department name • Allgemeines Krankenhaus der Stadt Wien - AKH
+					Department name • {name}
 				</div>
 				<div className='text-[#878D9D] flex'>
 					<img src={location} alt='' className='w-[13px] h-[18px] mr-4' />
-					Vienna, Austria
+					{address}
 				</div>
 			</div>
 			<div id='rating' className='flex items-center w-1/4 justify-end'>
 				<img src={stars} alt='' />
 			</div>
 			<div className='flex justify-end items-end w-1/4 relative'>
-				<img src={bookmark} alt='' className='absolute top-0' />
+				<img src={bookmark} alt='' className='absolute top-0 hidden md:block' />
 				<div id='postedTime' className='text-[#878D9D]'>
-					Posted 2 days ago
+					Posted {createdAt.slice(0, 10)}
 				</div>
 			</div>
 		</section>
